90d81bf50e01303787a164fef117303c277d72a3
Jiajie Chen <c@jia.je>
2022-07-24 13:38:48 +0800

    Use netlistsvg

--- src/synth_v.sh
+++ src/synth_v.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -x
-TEMP=$(cat /dev/urandom | base64 | env LC_CTYPE=C tr -cd 'a-f0-9' | head -c 32)
-OUTPUT_DOT=${1/.v/.dot}
 OUTPUT_PNG=${1/.v/.png}
 OUTPUT_SVG=${1/.v/.svg}
+OUTPUT_JSON=${1/.v/.json}
 OUTPUT_SYN_V=${1/.v/.syn.v}
-yosys -p "read_verilog $1" -p synth -p "abc -g AND,OR,XOR,NAND,NOR" -p "clean" -p "select -module $2" -p "show -prefix $TEMP -format png" -p "write_verilog ${OUTPUT_SYN_V}"
-mv ${TEMP}.dot ${OUTPUT_DOT}
-rm ${TEMP}.png
-python3 process.py ${OUTPUT_DOT}
-dot -Tpng ${OUTPUT_DOT} > ${OUTPUT_PNG}
-dot -Tsvg ${OUTPUT_DOT} > ${OUTPUT_SVG}
+yosys -p "read_verilog $1" -p synth -p "abc -g AND,OR,XOR,NAND,NOR" -p "clean" -p "select -module $2" -p "write_json ${OUTPUT_JSON}" -p "write_verilog ${OUTPUT_SYN_V}"
+netlistsvg ${OUTPUT_JSON} -o ${OUTPUT_SVG}
+rsvg-convert ${OUTPUT_SVG} -b white --height 800 --keep-aspect-ratio -o ${OUTPUT_PNG}
